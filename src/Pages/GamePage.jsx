92071db88b7f1b0f4b7924f4c4c60b585f809b51
yaellanir <yaellanir@gmail.com>
2022-11-21 14:50:05 +0200

    updated fetch api function for cards

--- src/Pages/GamePage.jsx
+++ src/Pages/GamePage.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React , {useState} from "react";
+import "./GamePage.css";
+import axios from "axios";
 
 function GamePage() {
-  return <div>GamePage</div>;
+  // const [cards , setCards ] = useState(null)
+  
+
+function shuffle() {
+    
+}
+
+
+  return <div className="gamepage-section">
+<div className="gameboard"></div>
+<div className="user-section"></div>
+    </div>;
 }
 
 export default GamePage;
